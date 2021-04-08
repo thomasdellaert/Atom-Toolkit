60e011bf4ca8b011de869c2f09f4687e15c7c633
Thomas Dellaert <dellaert.thomas@gmail.com>
2021-04-07 17:48:42 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,410 @@
+import pandas as pd
+import numpy as np
+import pint
+import pint_pandas
+import json
+import re
+
+ureg = pint.UnitRegistry()
+pint.set_application_registry(ureg)
+pint_pandas.PintType.ureg = ureg
+Q_ = ureg.Quantity
+pd.options.display.max_rows = 999
+
+c = pint.Context('spectroscopy')
+c.add_transformation('[wavenumber]', '[frequency]',
+                     lambda ureg, x: x * ureg.speed_of_light)
+c.add_transformation('[frequency]', '[wavenumber]',
+                     lambda ureg, x: x / ureg.speed_of_light)
+ureg.enable_contexts('spectroscopy')
+
+def load_NIST_data(species, term_ordered=True):
+    df = pd.read_csv(
+        'https://physics.nist.gov/cgi-bin/ASD/energy1.pl?de=0' +
+        '&spectrum=' + species.replace(' ', '+') +
+        '&submit=Retrieve+Data' +
+        '&units=0' +
+        '&format=2' +
+        '&output=0' +
+        '&page_size=15' +
+        '&multiplet_ordered=' + ('on' if term_ordered else '0') +
+        '&conf_out=on' +
+        '&term_out=on' +
+        '&level_out=on' +
+        '&unc_out=0' +
+        '&j_out=on' +
+        '&lande_out=on' +
+        '&perc_out=0' +
+        '&biblio=0' +
+        '&temp=',
+        index_col=False)
+    # strip the data of extraneous symbols
+    df_clean = df.applymap(lambda x: x.strip(' ="?'))
+    # coerce types
+    df_clean['Configuration'] = df_clean['Configuration'].astype('str')
+    df_clean['Term'] = df_clean['Term'].astype('str')
+    df_clean['Term'] = df_clean['Term'].apply(lambda x: re.sub(r'[a-z] ', '', x))
+    df_clean['J'] = df_clean['J'].astype('str')
+    df_clean['Level (cm-1)'] = pd.to_numeric(df_clean['Level (cm-1)'], errors='coerce')
+    if 'Lande' not in df_clean.columns:
+        df_clean['Lande'] = None
+    df_clean['Lande'] = pd.to_numeric(df_clean['Lande'], errors='coerce')
+
+    # drop rows that don't have a defined level
+    df_clean = df_clean.dropna(subset=['Level (cm-1)'])
+    # convert levels to a pint Quantity
+    df_clean['Level (cm-1)'] = df_clean['Level (cm-1)'].astype('pint[cm**-1]')
+    df_clean['Level (Hz)'] = df_clean['Level (cm-1)'].pint.to('Hz')
+
+    df_clean = df_clean[df_clean.J.str.contains("---") == False]
+    df_clean = df_clean[df_clean.J.str.contains(",") == False]
+
+    return df_clean
+
+
+class Term:
+    def __init__(self, conf, term, J, F=None, mF=None):
+
+        self.conf = conf
+        self.term = term
+        self.parity = (1 if '*' in self.term else 0)
+        #         if '/' not in term:
+        #             self.term  += self.float_to_frac(J)
+
+        self.J_frac = self.float_to_frac(J)
+        self.F_frac = self.float_to_frac(F)
+        self.mF_frac = self.float_to_frac(mF)
+
+        self.J = self.frac_to_float(J)
+        self.F = self.frac_to_float(F)
+        self.mF = self.frac_to_float(mF)
+
+        self.term_name = f'{self.term}{self.J_frac}'
+        if self.F is not None:
+            self.term_name += f' F={self.F_frac}'
+        if self.mF is not None:
+            self.term_name += f' mF={self.mF_frac}'
+
+        self.name = f'{self.conf} {self.term_name}'
+
+        self.coupling = self.get_coupling()
+
+        self.lc, self.sc, self.lo, self.so, self.jc, self.jo, self.l, self.s, self.k = self.get_quantum_nums()
+
+    def get_coupling(self):
+        if '[' in self.term:
+            if self.term[0] in 'SPDFGHIKMNOPQRTUVWXYZ':
+                return 'LK'
+            else:
+                return 'JK'
+        elif '(' in self.term:
+            return 'JJ'
+        elif len(self.term) > 1:
+            return 'LS'
+        else:
+            return "unknown"
+
+    def get_quantum_nums(self):
+        if self.coupling == 'LS':
+            l, s = self.parse_LS_term()
+            lc = sc = lo = so = jc = jo = k = None
+        elif self.coupling == 'JK':
+            lc, sc, lo, so, jc, k = self.parse_JK_term()
+            l = s = jo = None
+        elif self.coupling == 'JJ':
+            lc, sc, lo, so, jc, jo = self.parse_JJ_term()
+            l = s = k = None
+        elif self.coupling == 'LK':
+            lc = sc = lo = so = jc = jo = l = s = k = None
+            pass  # TODO: add LK term parsing
+        else:
+            lc = sc = lo = so = jc = jo = l = s = k = None
+        return lc, sc, lo, so, jc, jo, l, s, k
+
+    def parse_LS_term(self):
+        s = (int(re.search(r'^\d*', self.term).group()) - 1) / 2
+        l = self.let_to_l(re.search(r'[A-Z]', self.term).group())
+        return l, s
+
+    def parse_JK_term(self):
+        inner_terms = re.findall(r'\d[A-Z]', self.conf)
+        if len(inner_terms) == 2:
+            term_inner, term_outer = inner_terms
+            lc = self.let_to_l(term_inner[1])
+            sc = (int(term_inner[0]) - 1) / 2
+            lo = self.let_to_l(term_outer[1])
+            so = (int(term_outer[0]) - 1) / 2
+        elif len(inner_terms) == 1:
+            term_inner = inner_terms[0]
+            lc = self.let_to_l(term_inner[1])
+            sc = (int(term_inner[0]) - 1) / 2
+            lo = self.let_to_l(self.conf[-1])
+            so = 0
+        else:
+            lc = sc = lo = so = None
+
+        k = re.search(r'\[(.*?)]', self.term).group()
+        jc = re.search(r'<(.*?)>', self.conf).group()
+        k = self.frac_to_float(k.strip('[]'))
+        jc = self.frac_to_float(jc.strip('<>'))
+        return lc, sc, lo, so, jc, k
+
+    def parse_JJ_term(self):
+        # find the following forms: 3D<2>, 7p<3/2>, (8,5/2)*<21/2>
+        relevant_parts = re.findall(r'(((\d+[A-Za-z])|(\(.*?\)))\*?<(.*?)>)', self.conf)
+        if len(relevant_parts) == 0:  # sometimes the ancestor terms are in the term, not in the config
+            relevant_parts = re.findall(r'(((\d+[A-Za-z])|(\(.*?\)))\*?<(.*?)>)', self.term)
+        print(self.conf, self.term, relevant_parts)
+        jc = re.findall(r'<(.*?)>', relevant_parts[0])[0]
+        lc = self.let_to_l(re.search(r'[A-Z]', relevant_parts[0]).group(0))
+        sc = (int(re.search(r'\d+', relevant_parts[0]).group(0)) - 1)/2
+        jo = re.findall(r'<(.*?)>', relevant_parts[1])[0]
+        lo = self.let_to_l(re.search(r'[A-Z]', relevant_parts[1]).group(0))
+        so = (int(re.search(r'\d+', relevant_parts[1]).group(0)) - 1) / 2
+
+        '((?:(\d+)([A-Za-z]))|(?:\(.*?\)))\*?<(.*?)>'
+
+        """4f12.(3H<6>).5d.(2D).6s.6p.(3P*).(4F*<3/2>)  (6, 3/2)*
+3d9.(2D<5/2>).4p<3/2>  (5/2, 3/2)*
+5f4<7/2>.5f5<5/2>.(8,5/2)*<21/2>.7p<3/2>  (21/2, 3/2)
+5f3<7/2>.5f3<5/2>.(9/2, 9/2)<9>.7s.7p.(3P*<2>)  (9,2)*
+
+HOLY SHIT CAPTURE GROUPS ARE AMAZING"""
+        # js = re.findall(r'<(.*?)>', self.conf)
+        # if len(js) >= 2:
+        #     js = re.findall(r'(\d+/?\d*)', self.term)
+        #     inner_terms = re.findall(r'\d[A-Z]', self.conf)[-2:]
+        # else:
+        #     js = re.findall(r'<(.*?)>', self.term)
+        #     inner_terms = re.findall(r'\d[A-Z]', self.term)
+        #
+        # if len(inner_terms) == 2:
+        #     term_inner, term_outer = inner_terms
+        #     lc = self.let_to_l(term_inner[1])
+        #     sc = (int(term_inner[0]) - 1) / 2
+        #     lo = self.let_to_l(term_outer[1])
+        #     so = (int(term_outer[0]) - 1) / 2
+        # elif len(inner_terms) == 1:
+        #     term_inner = inner_terms[0]
+        #     lc = self.let_to_l(term_inner[1])
+        #     sc = (int(term_inner[0]) - 1) / 2
+        #     if self.conf[-1] == ')':
+        #         lo = re.findall(r'\d[A-Z]', self.conf)[-1][0]
+        #     else:
+        #         lo = self.let_to_l(re.sub(r'<(.*?)>', '', self.conf)[-1])
+        #     so = 0
+        # else:
+        #     lc = sc = lo = so = None
+        #
+        # jc, jo = js
+        jc = self.frac_to_float(jc.strip('<>'))
+        jo = self.frac_to_float(jo.strip('<>'))
+        return lc, sc, lo, so, jc, jo
+
+    @staticmethod
+    def let_to_l(let):
+        return 'SPDFGHIKLMNOQRTUVWXYZ'.index(let.upper())
+
+    @staticmethod
+    def l_to_let(l):
+        return 'SPDFGHIKLMNOQRTUVWXYZ'[l]
+
+    @staticmethod
+    def frac_to_float(frac):
+        if type(frac) == str:
+            if '/' in frac:
+                (f1, f2) = frac.split('/')
+                return float(f1) / float(f2)
+            else:
+                return float(frac)
+        if frac is None:
+            return None
+        return frac
+
+    @staticmethod
+    def float_to_frac(f):
+        # Assumes n/2, since all the fractions that appear in term symbols are of that form
+        if type(f) == str:
+            if '/' in f:
+                return f
+            try:
+                f = float(f)
+            except TypeError:
+                raise ValueError("Please input either a float or a fraction-formatted string")
+        if f is None:
+            return None
+        if (2 * f) % 2 == 0:
+            return str(int(f))
+        else:
+            return str(int(f * 2)) + '/2'
+
+
+class EnergyLevel:
+    def __init__(self, term, level, lande=None, parent=None, atom=None,
+                 depth='f', hfA=0.0, hfB=0.0, hfC=0.0):
+        self.term = term
+        self.level = level.to('Hz')
+        self.parent = parent
+        self.atom = atom
+        self.depth = depth
+        self.name = self.term.name
+        self.hfA, self.hfB, self.hfC = hfA, hfB, hfC
+        if lande is None:
+            self.lande = self.compute_gJ()
+        else:
+            self.lande = lande
+        if self.depth != 'f':
+            self.lande = self.compute_gF()
+        self._sublevels = {}
+        self.populate_sublevels()
+
+    def populate_sublevels(self):
+        if isinstance(self.parent, Atom):
+            for f in np.arange(abs(self.term.J - self.parent.I), self.term.J + self.parent.I + 1):
+                t = Term(self.term.conf, self.term.term, self.term.J, F=f)
+                shift = self.compute_hf_shift(f)
+                e = EnergyLevel(t, self.level + shift, lande=self.lande,
+                                parent=self, atom=self.atom, depth='hf',
+                                hfA=self.hfA, hfB=self.hfB, hfC=self.hfC)
+                self[f'F={f}'] = e
+        elif isinstance(self.parent, EnergyLevel):
+            if self.depth == 'hf':
+                for mf in np.arange(-self.term.F, self.term.F + 1):
+                    t = Term(self.term.conf, self.term.term, self.term.J, F=self.term.F, mF=mf)
+                    e = EnergyLevel(t, self.level, lande=self.lande,
+                                    parent=self, atom=self.atom, depth='z',
+                                    hfA=self.hfA, hfB=self.hfB, hfC=self.hfC)
+                    self[f'mF={mf}'] = e
+        else:
+            pass
+
+    def compute_hf_shift(self, F):
+        J = self.term.J
+        I = self.atom.I
+
+        IdotJ = 0.5 * (F * (F + 1) - J * (J + 1) - I * (I + 1))
+
+        FM1 = IdotJ
+
+        if J <= 0.5 or I <= 0.5:
+            FE2 = 0
+        else:
+            FE2 = (3 * IdotJ ** 2 + 1.5 * IdotJ - I * (I + 1) * J * (J + 1)) / (2.0 * I * (2.0 * I - 1.0) * J * (2.0 * J - 1.0))
+
+        if J <= 1 or I <= 1:
+            FM3 = 0
+        else:
+            FM3 = (10 * IdotJ ** 3 + 20 * IdotJ ** 2 + 2 * IdotJ * (
+                        -3 * I * (I + 1) * J * (J + 1) + I * (I + 1) + J * (J + 1) + 3)
+                   - 5 * I * (I + 1) * J * (J + 1)) / (I * (I - 1) * J * (J - 1) * (2 * J - 1))
+
+        return self.hfA * FM1 + self.hfB * FE2 + self.hfC * FM3
+
+    def compute_gF(self):
+        F = self.term.F
+        J = self.term.J
+        I = self.atom.I
+        if F != 0:
+            return self.lande * (F * (F + 1) + J * (J + 1) - I * (I + 1)) / (2 * F * (F + 1))
+        return 0
+
+    def compute_gJ(self):
+        if self.term.coupling != 'LS':
+            raise ValueError("Unable to compute g_J for non-LS-coupled terms")
+        J = self.term.J
+        L = self.term.l
+        S = self.term.s
+        return 1 + (J * (J + 1) + S * (S + 1) - L * (L + 1)) / (2 * J * (J + 1))
+
+    def __len__(self):
+        return len(self._sublevels)
+
+    def __getitem__(self, key):
+        return self._sublevels[key]
+
+    def __setitem__(self, key, level):
+        level.parent = self
+        self._sublevels[key] = level
+
+    def __delitem__(self, key):
+        del self._sublevels[key]
+
+    def __iter__(self):
+        return iter(self._sublevels)
+
+    def values(self):
+        return self._sublevels.values()
+
+    def keys(self):
+        return self._sublevels.keys()
+
+
+class Atom:
+    def __init__(self, name, I=0.0, levels=None):
+        self.name = name
+        self.I = Term.frac_to_float(I)
+        self._levels = {}
+        if levels is not None:
+            for level in levels:
+                self.append(level)
+
+    def __len__(self):
+        return len(self._levels)
+
+    def __getitem__(self, key):
+        return self._levels[key]
+
+    def __setitem__(self, key, level):
+        level.parent = self
+        level.atom = self
+        level.populate_sublevels()
+        self._levels[key] = level
+
+    def __delitem__(self, key):
+        del self._levels[key]
+
+    def __iter__(self):
+        return iter(self._levels)
+
+    def __str__(self):
+        return f'{self.name} I={Term.float_to_frac(self.I)}'
+
+    def __repr__(self):
+        return f"Atom({self.name}, I={self.I}, levels={self._levels})"
+
+    def append(self, level):
+        level.parent = self
+        level.atom = self
+        level.populate_sublevels()
+        self._levels[level.name] = level
+
+    def values(self):
+        return self._levels.values()
+
+    def keys(self):
+        return self._levels.keys()
+
+
+if __name__ == '__main__':
+    def energylevel_from_df(df, i):
+        t = Term(df["Configuration"][i], df["Term"][i], df["J"][i])
+        e = EnergyLevel(t, df["Level (cm-1)"][i], lande=df["Lande"][i], hfA=0.1 * ureg('megahertz'))
+        return e
+
+    species = "Yb II"
+    df = load_NIST_data(species)
+
+    a = Atom('171Yb', I=0.5)
+    for i in range(30):
+        try:
+            e = energylevel_from_df(df, i)
+            a.append(e)
+        except KeyError:
+            pass
+
+    for l in list(a.values()):
+        print('MAIN:', l.name, l.level.to('THz'))
+        for s in list(l.values()):
+            print('    SUB:', s.term.term_name, s.level.to('THz'))
+    #         for z in list(s.values()):
+    #             print('        Zee:', z.term.term_name)
---
+++ test_main.py
@@ -0,0 +1,95 @@
+from unittest import TestCase
+from main import Term
+import re
+
+
+class TestTerm(TestCase):
+    def test_get_coupling_LS(self):
+        term = Term('4f14.6s', '2S', '1/2')  # From Yb II
+        self.assertEqual(term.get_coupling(), 'LS')
+        term = Term('4f13.(2F*).6s2', '2F*', '5/2')  # From Yb II
+        self.assertEqual(term.get_coupling(), 'LS')
+        term = Term('3d8', '3F', '4')  # From Co II
+        self.assertEqual(term.get_coupling(), 'LS')
+        term = Term('5f3.6d.7s2', '2L', '6')  # From U I
+        self.assertEqual(term.get_coupling(), 'LS')
+        term = Term('5d10.5g', '2G', '9/2')  # From Hg II
+        self.assertEqual(term.get_coupling(), 'LS')
+
+    def test_get_coupling_JJ(self):
+        term = Term('5d9.6s.6p', '(2D<5/2>,3P<0>)*', '5/2')  # from Hg II
+        self.assertEqual(term.get_coupling(), 'JJ')
+        term = Term('4f13.(2F*<7/2>).6s.6p.(3P*<1>)', '(7/2,1)', '9/2')  # from Yb II
+        self.assertEqual(term.get_coupling(), 'JJ')
+        term = Term('5d9.6p2', '(2D<5/2>,1D<2>)', '7/2')  # from Hg II
+        self.assertEqual(term.get_coupling(), 'JJ')
+        term = Term('4f9.(6H*).5d.(7H*<8>).6s.6p.(3P*<0>)', '(8,0)', '8')  # from NIST examples
+        self.assertEqual(term.get_coupling(), 'JJ')
+        term = Term('4f12.(3H<6>).5d.(2D).6s.6p.(3P*).(4F*<3/2>)', '(6, 3/2)*', '13/2')
+        self.assertEqual(term.get_coupling(), 'JJ')
+        term = Term('3d9.(2D<5/2>).4p<3/2>', ' (5/2, 3/2)*', '3')
+        self.assertEqual(term.get_coupling(), 'JJ')
+        term = Term('5f4<7/2>.5f5<5/2>.(8,5/2)*<21/2>.7p<3/2>', '(21/2, 3/2)', '10')
+        self.assertEqual(term.get_coupling(), 'JJ')
+        term = Term('5f3<7/2>.5f3<5/2>.(9/2, 9/2)<9>.7s.7p.(3P*<2>)', '(9,2)*', '7')
+        self.assertEqual(term.get_coupling(), 'JJ')
+
+    def test_get_coupling_LK(self):
+        term = Term('3s2.3p.(2P*).4f', 'G 2[7/2]', '3')  # from NIST examples
+        self.assertEqual(term.get_coupling(), 'LK')
+        term = Term('3d7.(4P).4s.4p.(3P*)', 'D* 3[5/2]*', '7/2')
+        self.assertEqual(term.get_coupling(), 'LK')
+        term = Term('3s<2>.3p.(2P*).4f', 'G 2[7/2]', '3')
+        self.assertEqual(term.get_coupling(), 'LK')
+        # term = Term('3s<2>.3p.(2P*).4f', '5 2[7/2]', '3')
+        # self.assertEqual(term.get_coupling(), 'LK')
+
+    def test_get_coupling_JK(self):
+        term = Term('3p5.(2P*<1/2>).5g', '2[9/2]*', '5')  # from NIST examples
+        self.assertEqual(term.get_coupling(), 'JK')
+        term = Term('4f2.(3H<4>).5g', '2[3]', '5/2')  # from NIST examples
+        self.assertEqual(term.get_coupling(), 'JK')
+        term = Term('4f13.(2F*<7/2>)5d2.(1D)', '1[7/2]*', '7/2')  # from NIST examples
+        self.assertEqual(term.get_coupling(), 'JK')
+        term = Term('4f13.(2F*<5/2>).5d.6s.(3D)', '3[9/2]*', '11/2')  # from NIST examples
+        self.assertEqual(term.get_coupling(), 'JK')
+
+    def test_get_quantum_nums(self):
+        # LS terms
+        term = Term('4f14.6s', '2S', '1/2')  # From Yb II
+        self.assertEqual(term.get_quantum_nums(), (None, None, None, None, None, None, 0, 0.5, None))
+        term = Term('4f13.(2F*).6s2', '2F*', '5/2')  # From Yb II
+        self.assertEqual(term.get_quantum_nums(), (None, None, None, None, None, None, 3, 0.5, None))
+        term = Term('3d8', '3F', '4')  # From Co II
+        self.assertEqual(term.get_quantum_nums(), (None, None, None, None, None, None, 3, 1.0, None))
+        term = Term('5f3.6d.7s2', '2L', '6')  # From U I
+        self.assertEqual(term.get_quantum_nums(), (None, None, None, None, None, None, 8, 0.5, None))
+        term = Term('5d10.5g', '2G', '9/2')  # From Hg II
+        self.assertEqual(term.get_quantum_nums(), (None, None, None, None, None, None, 4, 0.5, None))
+        # JJ terms
+        term = Term('5d9.6s.6p', '(2D<5/2>,3P<0>)*', '5/2')  # from Hg II
+        self.assertEqual(term.get_quantum_nums(), (2, 0.5, 1, 1.0, 2.5, 0.0, None, None, None))
+        term = Term('4f13.(2F*<7/2>).6s.6p.(3P*<1>)', '(7/2,1)', '9/2')  # from Yb II
+        self.assertEqual(term.get_quantum_nums(), (3, 0.5, 1, 1.0, 3.5, 1.0, None, None, None))
+        term = Term('5d9.6p2', '(2D<5/2>,1D<2>)', '7/2')  # from Hg II
+        self.assertEqual(term.get_quantum_nums(), (2, 0.5, 2, 0.0, 2.5, 2.0, None, None, None))
+        term = Term('4f9.(6H*).5d.(7H*<8>).6s.6p.(3P*<0>)', '(8,0)', '8')  # from NIST examples
+        self.assertEqual(term.get_quantum_nums(), (5, 3.0, 1, 1.0, 8.0, 0.0, None, None, None))
+        term = Term('4f12.(3H<6>).5d.(2D).6s.6p.(3P*).(4F*<3/2>)', '(6, 3/2)*', '13/2')
+        self.assertEqual(term.get_quantum_nums(), (5, 1.0, 3, 1.5, 6.0, 1.5, None, None, None))
+        term = Term('3d9.(2D<5/2>).4p<3/2>', ' (5/2, 3/2)*', '3')
+        self.assertEqual(term.get_quantum_nums(), (2, 0.5, 1, 0.0, 2.5, 1.5, None, None, None))
+        term = Term('5f4<7/2>.5f5<5/2>.(8,5/2)*<21/2>.7p<3/2>', '(21/2, 3/2)', '10')
+        self.assertEqual(term.get_quantum_nums(), (None, None, 1, 0.0, 10.5, 1.5, None, None, None))
+        term = Term('5f3<7/2>.5f3<5/2>.(9/2, 9/2)<9>.7s.7p.(3P*<2>)', '(9,2)*', '7')
+        self.assertEqual(term.get_quantum_nums(), (None, None, 1, 1.0, 9.0, 2.0, None, None, None))
+        # JK terms
+        term = Term('3p5.(2P*<1/2>).5g', '2[9/2]*', '5')  # from NIST examples
+        self.assertEqual(term.get_quantum_nums(), (1, 0.5, 4, 0.5, 0.5, None, None, None, 4.5))
+        term = Term('4f2.(3H<4>).5g', '2[3]', '5/2')  # from NIST examples
+        self.assertEqual(term.get_quantum_nums(), (5, 1.5, 4, 0.5, 1.5, None, None, None, 3.0))
+        term = Term('4f13.(2F*<7/2>).5d2.(1D)', '1[7/2]*', '7/2')  # from NIST examples
+        self.assertEqual(term.get_quantum_nums(), (3, 0.5, 2, 0.0, 3.5, None, None, None, 3.5))
+        term = Term('4f13.(2F*<5/2>).5d.6s.(3D)', '3[9/2]*', '11/2')  # from NIST examples
+        self.assertEqual(term.get_quantum_nums(), (3, 0.5, 2, 1.0, 2.5, None, None, None, 4.5))
+        # LK terms
