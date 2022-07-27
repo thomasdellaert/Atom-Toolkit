aa2a7375c7e5a31b56d2f98e692d2082f45743d1
thomasdellaert <thomasdellaert@physics.ucla.edu>
2022-07-26 18:26:49 -0700

    beginnings of a grotrian class

--- atomtoolkit/render/grotrian.py
+++ atomtoolkit/render/grotrian.py
@@ -2,8 +2,9 @@
 Eventually meant for drawing Geotrian diagrams, perhaps even modifiable ones
 """
 
-import matplotlib.pyplot as plt
+import matplotlib
 import networkx as nx
+import pandas as pd
 
 def draw_levels(atom, plot_type='norm', **kwargs):
     """
@@ -24,35 +25,62 @@ def draw_levels(atom, plot_type='norm', **kwargs):
             pass
     nx.draw(model, pos=posdict, node_shape="_", with_labels=True, font_size=8, edge_color=(0., 0., 0., 0.2), **kwargs)
 
+RENDER_METHODS={'matplotlib', 'bokeh'}
+RENDERER = 'matplotlib'
 
-# class Grotrian:
-#     def __init__(self):
-#         self.ax = plt.ax
-#
-#
-# class LevelRenderer:
-#
-#     def __init__(self, level, parent):
-#         self.level = level
-#         self.parent = parent
-#         self.ax = self.parent.ax
-#         self.modes = {self.draw_level, self.draw_hf, self.draw_z, self.draw_abstract}
-#         self.draw_mode = self.draw_level
-#
-#     def mode(self, mode):
-#         if mode in self.modes:
-#             self.draw_mode = mode
-#         else:
-#             raise KeyError(f"mode not permitted. please use a mode in {self.modes}")
-#
-#     def draw_level(self):
-#         self.ax.hlines(self.level.level_hz, self.level.term.J-0.5, self.level.term.J+0.5)
-#
-#     def draw_hf(self):
-#         self.ax.hlines([hflevel.level_hz for hflevel in self.level.sublevels], self.level.term.J - 0.5, self.level.term.J + 0.5)
-#
-#     # def draw_z(self):
-#     #     for hflevel in self.level.sublevels:
-#     #         self.ax.hlines([zlevel.level_hz for zlevel in hflevel.sublevels], )
-#
-#
+class Grotrian:
+    level_color = 'k'
+    transition_color = 'spectrum'
+    level_ordering = 'J'
+    level_width = 0.8
+    add_level_behavior = 'gross'
+
+    def __init__(self):
+        self.level_color = Grotrian.level_color
+        self.transition_color = Grotrian.transition_color
+        self.level_ordering = Grotrian.level_ordering
+        self.level_width = Grotrian.level_width
+        self.add_level_behavior = Grotrian.add_level_behavior
+
+        self._add_level_behavior = {'hf': False, 'z': False}
+        if self.add_level_behavior == 'hyperfine':
+            self._add_level_behavior = {'hf': True, 'z': False}
+        elif self.add_level_behavior == 'zeeman':
+            self._add_level_behavior = {'hf': True, 'z': True}
+
+        self.levels_df = pd.DataFrame(columns=['level', 'hf', 'hfs', 'z', 'zs', 'w', 'x0', 'x1', 'kwargs'])
+        self.transition_df = pd.DataFrame(columns=['l0', 'l1', 'substructure', 'bold', 'color_func', 'kwargs'])
+
+    def add_level(self, level,
+                  hyperfine=None, hf_scale_factor=1e4,
+                  zeeman=None, z_scale_factor=1e3,
+                  width=None,
+                  position_offset=(0., 0.),
+                  position_override=None, **kwargs):
+        if hyperfine is None: hyperfine = self._add_level_behavior['hf']
+        if zeeman is None: zeeman = self._add_level_behavior['z']
+        if width is None: width = self.level_width
+
+        args = {'level': level,
+                'hf':hyperfine, 'hfs': hf_scale_factor,
+                'z': zeeman, 'zs': z_scale_factor,
+                'w': width,
+                'x1': position_offset, 'x0': position_override,
+                'kwargs': kwargs}
+        pd.concat(self.levels_df, pd.DataFrame(args))
+
+    def add_transition(self, l0, l1, substructure=False, bold=False, color_func=None, **kwargs):
+        if color_func is None: color_func = self.transition_color
+
+        args = {'l0': l0, 'l1': l1,
+                'substructure':substructure,
+                'bold': bold,
+                'color_func': color_func,
+                'kwargs': kwargs}
+        pd.concat(self.transition_df, pd.DataFrame(args))
+
+
+class MPLGrotrianRenderer:
+    def render(self, grotrian, fig=None, axes=None, **kwargs):
+        self.render_levels()
+        self.render_transitions()
