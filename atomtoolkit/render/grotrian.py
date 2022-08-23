0dbc4eac8e862878f2926e19356563060c6f5779
thomas' macbook <dellaert.thomas@gmail.com>
2022-08-23 00:35:08 -0700

    heavy modifications to Grotrian, with basic plotting implemented. Up next: position overrides and passing kwargs

--- atomtoolkit/render/grotrian.py
+++ atomtoolkit/render/grotrian.py
@@ -1,9 +1,13 @@
 """
 Eventually meant for drawing Geotrian diagrams, perhaps even modifiable ones
 """
+from abc import ABC, abstractmethod
 
+import matplotlib.pyplot as plt
+import matplotlib
 import networkx as nx
 import pandas as pd
+from ..atom import BaseLevel, BaseTransition
 
 
 def draw_levels(atom, plot_type='norm', **kwargs):
@@ -30,12 +34,106 @@ RENDER_METHODS = {'matplotlib', 'bokeh'}
 RENDERER = 'matplotlib'
 
 
+def gross_structure_level_table(caller, level: BaseLevel, **kwargs):
+    level = level.manifold
+
+    width = kwargs.pop('width', caller.level_width)
+    x0 = kwargs.pop('x0', caller.calculate_x0(level))
+    x1 = kwargs.pop('x1', 0)
+    y = level.manifold.level_Hz
+    return [[((x0 + x1) - width / 2, y), ((x0 + x1) + width / 2, y), kwargs]]
+
+
+def hf_stack_level_table(caller, level: BaseLevel, **kwargs):
+    level = level.manifold
+
+    width = kwargs.pop('width', caller.level_width)
+    x0 = kwargs.pop('x0', caller.calculate_x0(level))
+    x1 = kwargs.pop('x1', 0)
+    hf_scale = kwargs.pop('hf_scale', 10000.0)
+    lvls = []
+    for sublevel in level.sublevels():
+        y = level.level_Hz + hf_scale * sublevel.shift_Hz
+        lvls.append(
+            [((x0 + x1) - width / 2, y),
+             ((x0 + x1) + width / 2, y),
+             kwargs])
+    return lvls
+
+
+def zeeman_level_table(caller, level: BaseLevel, **kwargs):
+    level = level.manifold
+
+    width = kwargs.pop('width', caller.level_width)
+    x0 = kwargs.pop('x0', caller.calculate_x0(level))
+    x1 = kwargs.pop('x1', 0)
+    hf_scale = kwargs.pop('hf_scale', 10000.0)
+    z_scale = kwargs.pop('z_scale', 10.0)
+    fill_factor = kwargs.pop('fill_factor', 0.9)
+    lvls = []
+
+    deg_max = int(2 * (level.term.J + level.atom.I) + 1)
+
+    sublevel_width = fill_factor * width / deg_max
+    space_width = (1 - fill_factor) * width / (deg_max - 1)
+
+    for sublevel in level.sublevels():
+        for z_level in sublevel.sublevels():
+            y = level.level_Hz + hf_scale * sublevel.shift_Hz + z_scale * hf_scale * z_level.shift_Hz
+            lvls.append([
+                ((x0 + x1) + z_level.term.mF * (sublevel_width + space_width) - sublevel_width / 2, y),
+                ((x0 + x1) + z_level.term.mF * (sublevel_width + space_width) + sublevel_width / 2, y),
+                kwargs])
+
+    return lvls
+
+
+def hf_offset_level_table(caller, level: BaseLevel, **kwargs):
+    level = level.manifold
+
+    width = kwargs.pop('width', caller.level_width)
+    x0 = kwargs.pop('x0', caller.calculate_x0(level))
+    x1 = kwargs.pop('x1', 0)
+    sublevel_width = kwargs.pop('sublevel_width', width/1.5)
+    hf_scale = kwargs.pop('hf_scale', 10000.0)
+
+    slope = sublevel_width/len(level.sublevels())
+    lvls = []
+    for i, sublevel in enumerate(level.sublevels()):
+        y = level.level_Hz + hf_scale * sublevel.shift_Hz
+        lvls.append(
+            [((x0 + x1) - width / 2 + i * slope, y),
+             ((x0 + x1) + width / 2 + i * slope, y),
+             kwargs])
+    return lvls
+
+
+def hf_zeeman_like_level_table(caller, level: BaseLevel, **kwargs):
+    level = level.manifold
+
+    width = kwargs.pop('width', caller.level_width)
+    full_f_width = kwargs.pop('full_f_width', 4)
+    x0 = kwargs.pop('x0', caller.calculate_x0(level))
+    x1 = kwargs.pop('x1', 0)
+    hf_scale = kwargs.pop('hf_scale', 10000.0)
+
+    lvls = []
+    for i, sublevel in enumerate(level.sublevels()):
+        sublevel_width = width*(sublevel.term.F * 2 + 1)/(full_f_width * 2 + 1)
+        y = level.level_Hz + hf_scale * sublevel.shift_Hz
+        lvls.append(
+            [((x0 + x1) - sublevel_width / 2, y),
+             ((x0 + x1) + sublevel_width / 2, y),
+             kwargs])
+    return lvls
+
+
 class Grotrian:
     level_color = 'k'
     transition_color = 'spectrum'
     level_ordering = 'J'
     level_width = 0.8
-    add_level_behavior = 'gross'
+    add_level_strategy = gross_structure_level_table
     level_width_fluid = False
 
     def __init__(self):
@@ -43,36 +141,24 @@ class Grotrian:
         self.transition_color = Grotrian.transition_color
         self.level_ordering = Grotrian.level_ordering
         self.level_width = Grotrian.level_width
-        self.add_level_behavior = Grotrian.add_level_behavior
+        self.add_level_strategy = Grotrian.add_level_strategy
         self.level_width_fluid = Grotrian.level_width_fluid
 
-        self._add_level_behavior = {'hf': False, 'z': False}
-        if self.add_level_behavior == 'hyperfine':
-            self._add_level_behavior = {'hf': True, 'z': False}
-        elif self.add_level_behavior == 'zeeman':
-            self._add_level_behavior = {'hf': True, 'z': True}
-
-        self.levels_df = pd.DataFrame(columns=['level', 'hf', 'hfs', 'z', 'zs', 'w', 'x0', 'x1', 'kwargs'])
+        self.levels_df = pd.DataFrame(columns=['level', 'strategy', 'w', 'x1', 'x0', 'kwargs'])
         self.transition_df = pd.DataFrame(columns=['l0', 'l1', 'substructure', 'bold', 'color_func', 'kwargs'])
 
-    def add_level(self, level,
-                  hyperfine=None, hf_scale_factor=1e4,
-                  zeeman=None, z_scale_factor=1e3,
-                  width=None,
-                  position_offset=(0., 0.),
-                  position_override=None,
-                  **kwargs):
-        if hyperfine is None: hyperfine = self._add_level_behavior['hf']
-        if zeeman is None: zeeman = self._add_level_behavior['z']
-        if width is None: width = self.level_width
+    def add_level(self, level, strategy=None, **kwargs):
+        if strategy is None:
+            strategy = self.add_level_strategy
+        width = kwargs.pop('width', self.level_width)
+        x1 = kwargs.pop('position_offset', 0.0)
+        x0 = kwargs.pop('position_override', None)
 
-        args = {'level': level,
-                'hf': hyperfine, 'hfs': hf_scale_factor,
-                'z': zeeman, 'zs': z_scale_factor,
-                'w': width,
-                'x1': position_offset, 'x0': position_override,
-                'kwargs': kwargs}
-        pd.concat(self.levels_df, pd.DataFrame(args))
+        args = {'level': [level], 'strategy': [strategy],
+                'w': [width], 'x1': [x1], 'x0': [x0],
+                'kwargs': [kwargs]}
+
+        self.levels_df = pd.concat([self.levels_df, pd.DataFrame(args)], ignore_index=True)
 
     def add_transition(self, l0, l1,
                        substructure=False,
@@ -86,59 +172,33 @@ class Grotrian:
                 'bold': bold,
                 'color_func': color_func,
                 'kwargs': kwargs}
-        pd.concat(self.transition_df, pd.DataFrame(args))
+        pd.concat([self.transition_df, pd.DataFrame(args)])
 
     def calculate_x0(self, level):
         if self.level_ordering == 'J':
-            return level.J
-
-    def compute_lines_gross(self, level, x0, x1, width):
-        level = level.manifold
-        if x0 is None:
-            x0 = self.calculate_x0(level)
-
-        return [[level.manifold.level_Hz, (x0 + x1) - width / 2, (x0 + x1) + width / 2]]
-
-    def compute_lines_hf(self, level, x0, x1, width, scale_factor):
-        level = level.manifold
-        lvls = []
-        if x0 is None:
-            x0 = self.calculate_x0(level.manifold)
-
-        for sublevel in level.sublevels():
-            lvls.append([level.level_Hz + scale_factor * sublevel.shift_Hz, (x0 + x1) - width / 2, (x0 + x1) + width / 2])
-
-        return lvls
-
-    def compute_lines_z(self, level, x0, x1, width, fill_factor, hf_scale_factor, z_scale_factor):
-        level = level.manifold
-        lvls = []
-        if x0 is None:
-            x0 = self.calculate_x0(level.manifold)
-
-        deg_max = int(2 * (level.term.J + level.atom.I) + 1)
-
-        sublevel_width = fill_factor * width / deg_max
-        space_width = (1 - fill_factor) * width / (deg_max - 1)
-
-        for sublevel in level.sublevels():
-            for z_level in sublevel.sublevels():
-                lvls.append([
-                    level.level_Hz + hf_scale_factor * sublevel.shift_Hz + z_scale_factor * z_level.shift_Hz,
-                    (x0 + x1) + z_level.term.mF * (sublevel_width + space_width) - sublevel_width / 2,
-                    (x0 + x1) + z_level.term.mF * (sublevel_width + space_width) + sublevel_width / 2
-                ])
-
-        return lvls
+            return level.term.J
 
 
 class MPLGrotrianRenderer:
-    def render(self, grotrian, fig=None, axes=None, **kwargs):
-        self.render_levels()
-        self.render_transitions()
+    @classmethod
+    def render(cls, grotrian, axes=None, **kwargs):
+        if axes is None:
+            fig, axes = plt.subplots()
+        cls.render_levels(grotrian, axes)
+        cls.render_transitions()
 
-    def render_levels(self):
+    @classmethod
+    def render_levels(cls, grotrian, axes):
+        cols = ['p0', 'p1', 'kwargs']
+        df = pd.DataFrame(columns=cols)
+        for i, row in grotrian.levels_df.iterrows():
+            df = pd.concat([df, pd.DataFrame(data=row['strategy'](grotrian, row['level']), columns=cols)])
+        coords = list(zip(df['p0'].tolist(), df['p1'].tolist()))
+
+        lc = matplotlib.collections.LineCollection(coords)
+        axes.add_collection(lc)
+
+    @classmethod
+    def render_transitions(cls):
         pass
 
-    def render_transitions(self):
-        pass
