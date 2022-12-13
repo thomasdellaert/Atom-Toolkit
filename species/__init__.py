811abcee863f18610ecea6384c2d1e8f68d7680b
thomas' macbook <dellaert.thomas@gmail.com>
2022-12-12 16:16:02 -0800

    added a grocery list of species to add

--- species/__init__.py
+++ species/__init__.py
@@ -1,2 +1,23 @@
 # TODO: Look more deeply into namespace packaging and see if it's possible to include this package in the
 #  namespace of atomtoolkit in a nice way
+
+# TODO: 9Be+
+
+# TODO: 137Ba+
+
+# TODO: 133Ba+
+
+# TODO: 138Ba+
+
+# TODO: 89Sr+
+
+# TODO: 43Ca+
+
+# TODO: 40Ca+
+
+# TODO: 87Rb
+
+# TODO: 133Cs
+
+# TODO: Maybe if you try to import something that doesn't exist it could just throw a warning and try to do a quick
+#  NIST import?
